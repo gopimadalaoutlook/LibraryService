43e2a60ce3622257b271bdfd8f89add798e8e8b2
gopim <gopim@GOPIMADALA>
2025-04-25 10:38:29 -0500

    Add project files.

---
+++ LibraryService.WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LibraryService.WebAPI.Data;
+using LibraryService.WebAPI.Services;
+
+namespace LibraryService.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/libraries/{libraryId}/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILibrariesService _librariesService;
+        private readonly IBooksService _booksService;
+
+        public BooksController(IBooksService booksService, ILibrariesService librariesService)
+        {
+            _librariesService = librariesService;
+            _booksService = booksService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int libraryId)
+        {
+            var library = (await _librariesService.Get(new[] { libraryId })).FirstOrDefault();
+            if (library == null)
+                return NotFound();
+            var books = await _booksService.Get(libraryId);
+            return Ok(books);
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post(int libraryId, Book book)
+        {
+            var library = (await _librariesService.Get(new[] { libraryId })).FirstOrDefault();
+            if (library == null)
+                return NotFound();
+            book.LibraryId = libraryId;
+            var addedBook = await _booksService.Add(book);
+            return CreatedAtAction(nameof(Get), new { libraryId = libraryId, id = addedBook.Id }, addedBook);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(int libraryId, Book book)
+        {
+            var library = (await _librariesService.Get(new[] { libraryId })).FirstOrDefault();
+            if (library == null)
+                return NotFound();
+            book.LibraryId = libraryId;
+            var updatedBook = await _booksService.Update(book);
+            return Ok(updatedBook);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int libraryId, int id)
+        {
+            var book = (await _booksService.Get(libraryId)).FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound();
+            var deletedBook = await _booksService.Delete(book);
+            return Ok();
+        }
+    }
+}
---
+++ LibraryService.WebAPI/Services/BooksService.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryService.WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryService.WebAPI.Services
+{
+    public class BooksService : IBooksService
+    {
+        private readonly LibraryContext _libraryContext;
+
+        public BooksService(LibraryContext libraryContext)
+        {
+            _libraryContext = libraryContext;
+        }
+
+        public async Task<IEnumerable<Book>> Get(int libraryId)
+        {
+            var books = _libraryContext.Books.AsQueryable();
+            if (libraryId > 0)
+                books = books.Where(x => x.LibraryId == libraryId);
+            return await books.ToListAsync();
+        }
+
+        public async Task<Book> Add(Book book)
+        {
+            var library = await _libraryContext.Libraries.SingleOrDefaultAsync(x => x.Id == book.LibraryId);
+            if (library == null)
+                throw new Exception("Library not found");
+            await _libraryContext.Books.AddAsync(book);
+            await _libraryContext.SaveChangesAsync();
+            return book;
+        }
+
+        public async Task<Book> Update(Book book)
+        {
+            var existingBook = await _libraryContext.Books.SingleOrDefaultAsync(x => x.Id == book.Id);
+            if (existingBook == null)
+                throw new Exception("Book not found");
+            existingBook.Name = book.Name;
+            existingBook.Category = book.Category;
+            existingBook.LibraryId = book.LibraryId;
+            _libraryContext.Books.Update(existingBook);
+            await _libraryContext.SaveChangesAsync();
+            return existingBook;
+
+        }
+
+        public async Task<bool> Delete(Book book)
+        {
+            var existingBook = await _libraryContext.Books.SingleOrDefaultAsync(x => x.Id == book.Id);
+            if (existingBook == null)
+                throw new Exception("Book not found");
+            _libraryContext.Books.Remove(existingBook);
+            await _libraryContext.SaveChangesAsync();
+            return true;
+        }
+    }
+
+    public interface IBooksService
+    {
+        Task<IEnumerable<Book>> Get(int libraryId);
+
+        Task<Book> Add(Book book);
+
+        Task<Book> Update(Book book);
+
+        Task<bool> Delete(Book book);
+    }
+}
